7c4665bd4d1e15ce3da8da000a235f921c67c08e
Justin Aaron Natividad <justinaaron.natividad@benilde.edu.ph>
2023-08-06 23:04:45 +0800

    Lock On Select

--- NewAsteroids/Assets/Members/Natividad/Scripts/UI/Selection/SelectionGroup.cs
+++ NewAsteroids/Assets/Members/Natividad/Scripts/UI/Selection/SelectionGroup.cs
@@ -8,7 +8,11 @@ public class SelectionGroup : MonoBehaviour
     [SerializeField] private AssignedPlayerUI assignedPlayer;
     public SelectionCursor cursor;
 
+    [SerializeField] private bool lockCursorOnSelect;
+
     [SerializeField] private Selection activeSelection;
+
+    private bool isEnabled = true;
 
     private void OnEnable()
     {
@@ -27,22 +31,34 @@ public class SelectionGroup : MonoBehaviour
 
     private void SetControls(PlayerInputControls controls)
     {
-        Debug.Log("Assigned");
         switch (assignedPlayer)
         {
             case AssignedPlayerUI.Player1:
                 controls.Player1UI.Navigate.performed += MoveActiveSelection;
                 controls.Player1UI.Submit.performed += ActivateSelection;
+                controls.Player1UI.Cancel.performed += CancelSelection;
                 break;
             case AssignedPlayerUI.Player2:
                 controls.Player2UI.Navigate.performed += MoveActiveSelection;
                 controls.Player2UI.Submit.performed += ActivateSelection;
+                controls.Player2UI.Cancel.performed += CancelSelection;
                 break;
             default:
                 controls.GenericUI.Navigate.performed += MoveActiveSelection;
                 controls.GenericUI.Submit.performed += ActivateSelection;
+                controls.GenericUI.Cancel.performed += CancelSelection;
                 break;
         }
+    }
+
+    public void EnableGroup()
+    {
+        isEnabled = true;
+    }
+
+    public void DisableGroup()
+    {
+        isEnabled = false;
     }
 
     public void SetActiveSelection(Selection selection)
@@ -52,12 +68,21 @@ public class SelectionGroup : MonoBehaviour
 
     public void MoveActiveSelection(InputAction.CallbackContext context)
     {
+        if (!isEnabled) return;
         activeSelection?.MoveTo(context.ReadValue<Vector2>());
     }
 
     public void ActivateSelection(InputAction.CallbackContext context)
     {
+        if (!isEnabled) return;
         cursor.Pop();
         activeSelection?.Activate();
+        if (lockCursorOnSelect) DisableGroup();
+    }
+
+    public void CancelSelection(InputAction.CallbackContext context)
+    {
+        if (isEnabled) return;
+        EnableGroup();
     }
 }
